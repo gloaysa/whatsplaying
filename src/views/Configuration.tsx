80fdbea14afaba3260ee0f603ce378606b58a229
Guillermo Loaysa <gloaysa@gmail.com>
2024-03-16 17:49:52 +0100

    feat: trim plex token before storing to avoid trailing spaces

--- src/views/Configuration.tsx
+++ src/views/Configuration.tsx
@@ -1,18 +1,5 @@
-import React, {
-  useState,
-  useEffect,
-  FunctionComponent,
-  FormEvent,
-} from "react";
-import {
-  TextField,
-  Button,
-  Box,
-  Typography,
-  Card,
-  CardContent,
-  Grid,
-} from "@mui/material";
+import React, { useState, useEffect, FunctionComponent, FormEvent } from "react";
+import { TextField, Button, Box, Typography, Card, CardContent, Grid } from "@mui/material";
 
 export const Configuration: FunctionComponent = () => {
   const [plexToken, setPlexToken] = useState("");
@@ -26,22 +13,18 @@ export const Configuration: FunctionComponent = () => {
 
     if (storedPlexToken) setPlexToken(storedPlexToken);
     if (storedHideLibraries) setHideLibraries(storedHideLibraries.split(","));
-    if (storedPreferredOrder)
-      setPreferredOrder(storedPreferredOrder.split(","));
+    if (storedPreferredOrder) setPreferredOrder(storedPreferredOrder.split(","));
   }, []);
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    localStorage.setItem("plexToken", plexToken);
+    localStorage.setItem("plexToken", plexToken.trimStart().trimEnd());
     localStorage.setItem("hideLibraries", hideLibraries.join(","));
     localStorage.setItem("preferredOrder", preferredOrder.join(","));
     window.location.assign("/");
   };
 
-  const handleAddField = (
-    setter: React.Dispatch<React.SetStateAction<string[]>>,
-    fields: string[],
-  ) => {
+  const handleAddField = (setter: React.Dispatch<React.SetStateAction<string[]>>, fields: string[]) => {
     setter([...fields, ""]);
   };
 
@@ -78,15 +61,12 @@ export const Configuration: FunctionComponent = () => {
         <form onSubmit={handleSubmit}>
           <CardContent>
             <Typography variant="body1" sx={{ paddingBottom: "15px" }}>
-              What's Playing can be customized to suit your needs by setting
-              some configurations, like what Plex libraries you would like to
-              hide from your Album showcase (you maybe have a library of type
-              music for audiobooks or Christmas music to not have it merged with
-              your normal music), what order you would like to display your
-              media players in (maybe you are using this for an specific room
-              and the speaker in that room should have precedence over all
-              others), and your Plex Token (depending on the user of the token,
-              you will display the info when they are plugin music).
+              What's Playing can be customized to suit your needs by setting some configurations, like what Plex
+              libraries you would like to hide from your Album showcase (you maybe have a library of type music for
+              audiobooks or Christmas music to not have it merged with your normal music), what order you would like to
+              display your media players in (maybe you are using this for an specific room and the speaker in that room
+              should have precedence over all others), and your Plex Token (depending on the user of the token, you will
+              display the info when they are plugin music).
             </Typography>
             <Grid container spacing={2}>
               <Grid item xs={12}>
@@ -102,43 +82,20 @@ export const Configuration: FunctionComponent = () => {
               <Grid item xs={12}>
                 <Typography variant="h6">Hide Libraries</Typography>
                 <Typography variant="body1" sx={{ paddingBottom: "5px" }}>
-                  For example, if you have libraries named "Music", "AudioBooks"
-                  and "Podcasts" and you want to hide the "AudioBooks" and
-                  "Podcasts" libraries, you would will add two new entries with
-                  names of each library.
+                  For example, if you have libraries named "Music", "AudioBooks" and "Podcasts" and you want to hide the
+                  "AudioBooks" and "Podcasts" libraries, you would will add two new entries with names of each library.
                 </Typography>
                 {hideLibraries.map((lib, index) => (
                   <Box key={index} sx={{ marginBottom: "1%" }}>
                     <TextField
                       value={lib}
-                      onChange={(e) =>
-                        handleFieldChange(
-                          index,
-                          e.target.value,
-                          setHideLibraries,
-                          hideLibraries,
-                        )
-                      }
+                      onChange={(e) => handleFieldChange(index, e.target.value, setHideLibraries, hideLibraries)}
                       fullWidth
                     />
-                    <Button
-                      onClick={() =>
-                        handleRemoveField(
-                          index,
-                          setHideLibraries,
-                          hideLibraries,
-                        )
-                      }
-                    >
-                      -
-                    </Button>
+                    <Button onClick={() => handleRemoveField(index, setHideLibraries, hideLibraries)}>-</Button>
                   </Box>
                 ))}
-                <Button
-                  onClick={() =>
-                    handleAddField(setHideLibraries, hideLibraries)
-                  }
-                >
+                <Button onClick={() => handleAddField(setHideLibraries, hideLibraries)}>
                   Add a new library to hide
                 </Button>
               </Grid>
@@ -146,58 +103,27 @@ export const Configuration: FunctionComponent = () => {
               <Grid item xs={12}>
                 <Typography variant="h6">Preferred Order</Typography>
                 <Typography variant="body1" sx={{ paddingBottom: "5px" }}>
-                  List the device's names in the order you prefer them to be
-                  displayed. For example, if you have devices named "Living
-                  Room", "Bedroom", and "Kitchen" and you want them to be
-                  displayed in that order, you would add a new entry in said
-                  order for each device. If none of them are playing, but you
-                  have another device playing, the currently playing will be
-                  displayed by default. As soon as any of your favorite devices
-                  start playing, it will be displayed instead.
+                  List the device's names in the order you prefer them to be displayed. For example, if you have devices
+                  named "Living Room", "Bedroom", and "Kitchen" and you want them to be displayed in that order, you
+                  would add a new entry in said order for each device. If none of them are playing, but you have another
+                  device playing, the currently playing will be displayed by default. As soon as any of your favorite
+                  devices start playing, it will be displayed instead.
                 </Typography>
                 {preferredOrder.map((order, index) => (
                   <Box key={index} sx={{ marginBottom: "1%" }}>
                     <TextField
                       value={order}
-                      onChange={(e) =>
-                        handleFieldChange(
-                          index,
-                          e.target.value,
-                          setPreferredOrder,
-                          preferredOrder,
-                        )
-                      }
+                      onChange={(e) => handleFieldChange(index, e.target.value, setPreferredOrder, preferredOrder)}
                       fullWidth
                     />
-                    <Button
-                      onClick={() =>
-                        handleRemoveField(
-                          index,
-                          setPreferredOrder,
-                          preferredOrder,
-                        )
-                      }
-                    >
-                      -
-                    </Button>
+                    <Button onClick={() => handleRemoveField(index, setPreferredOrder, preferredOrder)}>-</Button>
                   </Box>
                 ))}
-                <Button
-                  onClick={() =>
-                    handleAddField(setPreferredOrder, preferredOrder)
-                  }
-                >
-                  Add a new device
-                </Button>
+                <Button onClick={() => handleAddField(setPreferredOrder, preferredOrder)}>Add a new device</Button>
               </Grid>
 
               <Grid item xs={12}>
-                <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  disabled={!plexToken}
-                >
+                <Button type="submit" fullWidth variant="contained" disabled={!plexToken}>
                   Save
                 </Button>
               </Grid>
